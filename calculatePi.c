e1bdcd109804481dbdec7f2ca168863b87a757fd
Justin135 <justin.bornais@gmail.com>
2021-08-03 23:22:01 -0400

    Added first implementation of number arrays. Addition doesn't yet work.

--- calculatePi.c
+++ calculatePi.c
@@ -1,27 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 #define LIMIT 10
+
+char * addNums(char x[], char y[]);
+char * addNums(char x[], char y[]) {
+    
+    char * sum;
+    if(strlen(x) >= strlen(y)) sum = malloc((strlen(x) + 1) * sizeof(char));
+    else sum = malloc((strlen(y) + 1) * sizeof(char));
+    
+    int i = strlen(x) - 1;
+    int j = strlen(y) - 1;
+    int k = strlen(sum) - 1;
+    int d, carry = 0;
+    
+    while(i >= 0 && j >= 0) {
+        
+        d = (x[i] - 48) + (y[j] - 48) + carry;
+        carry = (d / 10);
+        sum[k] = (d % 10) + 48;
+        printf("%c ", sum[k]);
+        i--;
+        j--;
+        k--;
+    }
+    
+    while(i >= 0) {
+        d = (x[i] - 48) + carry;
+        carry = d / 10;
+        sum[k] = (d % 10);
+        
+        i--;
+        k--;
+    }
+    
+    while(j >= 0) {
+        d = (y[j] - 48) + carry;
+        carry = d / 10;
+        sum[k] = (d % 10);
+        
+        j--;
+        k--;
+    }
+    
+    return sum;
+}
 
 char * calculatePi(int limit);
 char * calculatePi(int limit) {
     
     char * pi = malloc(limit * sizeof(char));
-    char digit = '\0';
-    double d = 0;
-    
-    for(int k = 0;k < limit;k++) {
-        
-        d = (1 / pow(16, k))
-            * ((4.0 / ((8.0 * k) + 1))
-            - (2.0 / ((8.0 * k) + 4))
-            - (1 / ((8.0 * k) + 5))
-            - (1 / ((8.0 * k) + 6))
-            );
-        
-        printf("%lf\n", d);
-    }
     
     return pi;
 }
@@ -29,5 +60,19 @@ char * calculatePi(int limit) {
 int main(void) {
     
     char * pi = calculatePi(LIMIT);
+    
+    char num = '9';
+    int nums = num - 48;
+    
+    char x[5] = "12345";
+    char y[5] = "23456";
+    char * z = addNums(x, y);
+    
+    printf("\n%s\n", z);
+    free(z);
+    free(pi);
+    
+    puts("All good.");
+    
     return 0;
 }
